8071c36b053dc1a626bec8d8f503618d3dc03caa
4dTailor <91608035+4dTailor@users.noreply.github.com>
2024-04-27 12:45:03 +1000

    added more data
    Signed-off-by: 4dTailor <91608035+4dTailor@users.noreply.github.com>

--- .bashrc
+++ .bashrc
@@ -119,6 +119,8 @@ fi
 
 export PATH=$PATH:~/.local/bin
 
+neofetch
+
 # Init Oh My Posh
 # eval "$(oh-my-posh init bash)"
-eval "$(oh-my-posh init bash --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/craver.omp.json)"
+eval "$(oh-my-posh init bash --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/di4am0nd.omp.json)"
--- install.sh
+++ install.sh
@@ -1,24 +1,38 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+SCRIPT_NAME="SQ Script"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# == COLOR CODING ==
+CL_WARN="\e[33m"
 
 # Install Oh My Posh on the system
 mkdir -p ~/.local/bin ~/.local/lib
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+if [ ! -f ~/.local/bin/oh-my-posh ]; then
+    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+fi
 
+FONT_LOCATION="~/.local/share/fonts"
 # Install a nerd font
-if [ -d "~/.local/share/fonts" ]; then
-    mkdir -p ~/.local/share/fonts
-    cd ~/.local/share/fonts
+if [ ! -d $FONT_LOCATION/Agave ]; then
+    mkdir -p $FONT_LOCATION/Agave
+    echo -e "$SCRIPT_NAME - ${CL_WARN}[WARN] Installing unzip"
     sudo apt install unzip
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Agave.zip
-    unzip Agave.zip
-    fc-cache -f -v
-    rm Agave.zip
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Agave.zip" $FONT_LOCATION/Agave
+    unzip $FONT_LOCATION/Agave/Agave.zip
+    fc-cache -v
+    rm $FONT_LOCATION/Agave/Agave.zip
 fi
 
 # Create symbolic links with files in the dotfiles directory (NB: Replaces existing files)
-ln -sf $PWD/.bashrc ~/.bashrc 
-ln -sf $PWD/.gitconfig ~/.gitconfig
-echo "Successfully set symbolic links to files"
+echo -e "$SCRIPT_NAME - ${CL_WARN}[INFO] Symlinking config files"
+ln -sf $SCRIPT_DIR/.bashrc ~/.bashrc 
+ln -sf $SCRIPT_DIR/.gitconfig ~/.gitconfig
 
+if [ -e /etc/i3 ]; then
+    ln -sf $SCRIPT_DIR/i3/config ~/.config/i3/config
+else
+    echo -e "$SCRIPT_NAME - ${CL_WARN}[WARN] I3 not installed, not adding I3 config"
+fi
 
-echo "Remember to set Agave Nerd Font Mono to the terminal"
+echo -e "$SCRIPT_NAME - [INFO] Remember to set Agave Nerd Font Mono to the terminal"
