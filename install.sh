194a33cfb7d8bc82c802ab6f64d4353c465af10f
4dTailor <91608035+4dTailor@users.noreply.github.com>
2024-04-29 12:30:17 +1000

    Updated install.sh to check if packages exist

--- install.sh
+++ install.sh
@@ -3,23 +3,30 @@
 SCRIPT_NAME="SQ Script"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# == COLOR CODING ==
-CL_WARN="\e[33m"
+if [ ! $(command -v curl) ]; then
+    echo "$SCRIPT_NAME - [ERROR] curl is not installed in your system"
+    exit 1
+fi
+
+if [ ! $(command -v unzip) ]; then
+    echo "$SCRIPT_NAME - [ERROR] unzip is not installed in your system"
+    exit 1
+fi
+
 
 # Install Oh My Posh on the system
+echo "$SCRIPT_NAME - Installing oh-my-posh"
 mkdir -p ~/.local/bin ~/.local/lib
 if [ ! -f ~/.local/bin/oh-my-posh ]; then
     curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 fi
 
-FONT_LOCATION="~/.local/share/fonts"
+FONT_LOCATION=~/.local/share/fonts
 # Install a nerd font
 if [ ! -d $FONT_LOCATION/Agave ]; then
     mkdir -p $FONT_LOCATION/Agave
-    echo -e "$SCRIPT_NAME - ${CL_WARN}[WARN] Installing unzip"
-    sudo apt install unzip
-    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Agave.zip" $FONT_LOCATION/Agave
-    unzip $FONT_LOCATION/Agave/Agave.zip
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Agave.zip" -P $FONT_LOCATION/Agave
+    unzip $FONT_LOCATION/Agave/Agave.zip -d $FONT_LOCATION/Agave
     fc-cache -v
     rm $FONT_LOCATION/Agave/Agave.zip
 fi
